073cd149c8d21fbab2451c74626df973450f5b57
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-19 15:07:59 -0700

    back end init set up finished

---
+++ seed.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+const seedData = require('./test/seedData.js');
+const mongoose = require('mongoose');
+
+seedData()
+  .then(() => console.log('done'))
+  .finally(() => mongoose.connection.close());
