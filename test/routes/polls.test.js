af4d6d9f9ea4da79c8d60c02f0d061b30785990c
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-19 15:45:16 -0700

    added seed data dropping and generating to routes test

--- test/routes/polls.test.js
+++ test/routes/polls.test.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
 require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
+const seedData = require('../seedData');
 
 describe('polls route', () => {
+  beforeEach(() => {
+    return seedData();
+  });
+
+  afterEach(done => {
+    mongoose.connection.dropDatabase(done);
+    
+  });
+
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
   it('posts a new poll', () => {
     return request(app) 
       .post('/polls')
